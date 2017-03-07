10a46ca2067ba8b203024b9d2ff0b72a304c23b7
Sergey Monakhov <monakhov@ordbuy.com>
2017-03-07 10:31:07 +0000

    fix conn()

--- check.go
+++ check.go
@@ -19,11 +19,11 @@ type alertMessage struct {
 type connection struct {
 	protocol string
 	host     string
-	port     string
+	port     int
 	address  string
 }
 
-func checkError(err error) (int) {
+func checkError(err error) int {
 	if err == nil {
 		return 0
 	}
@@ -31,10 +31,10 @@ func checkError(err error) (int) {
 	return 1
 }
 
-func (c *connection) conn() (net.Conn, error) {
+func (c *connection) conn() (net.Conn, int) {
 	conn, err := net.DialTimeout(c.protocol, c.address, 3*time.Second)
-	checkError(err)
-	return conn, err
+	errInt := checkError(err)
+	return conn, errInt
 }
 
 func (a *alertMessage) sentSlack() {
@@ -56,12 +56,12 @@ func (a *alertMessage) sentSlack() {
 
 func main() {
 	var (
-		k 	string
-		t	string
-		lastE	int = -1
+		k        string
+		t        string
+		lastE    int
 		protocol = flag.String("protocol", "tcp", "protocol tcp/udp")
 		host     = flag.String("host", "ya.ru", "destination host")
-		port     = flag.String("port", "80", "destination port")
+		port     = flag.Int("port", 80, "destination port")
 		interval = flag.Uint("interval", 5, "interval check seconds")
 		url      = flag.String("url", "", "hook url")
 	)
@@ -71,27 +71,26 @@ func main() {
 	for {
 		c := connection{
 			protocol: *protocol,
-			address:  fmt.Sprintf("%s:%s", *host, *port),
+			address:  fmt.Sprintf("%s:%v", *host, *port),
 		}
 		conn, err := c.conn()
-		e := checkError(err)
-                if e != lastE {
-                        if e == 0 {		// normal
-                                k = "good"
-                                t = "reachable"
-                                conn.Close()
-                        } else {		// not normal
-                                k = "danger"
-                                t = "unreachable"
-                        }
-                        lastE = e		// key of success
-                        am := alertMessage{
-                                color:  k,
-				text:   fmt.Sprintf("Destination host %s:%s %s\n", *host, *port, t),
-                                url:    *url,
-                        }
-                        am.sentSlack()
-                }
+		if err != lastE {
+			if err == 0 { // normal
+				k = "good"
+				t = "reachable"
+				conn.Close()
+			} else { // not normal
+				k = "danger"
+				t = "unreachable"
+			}
+			lastE = err // key of success
+			am := alertMessage{
+				color: k,
+				text:  fmt.Sprintf("Destination host %s:%v %s\n", *host, *port, t),
+				url:   *url,
+			}
+			am.sentSlack()
+		}
 		time.Sleep(time.Duration(*interval) * time.Second)
 	}
 }
